74bd369d3358ef3ec5436b89ad48bae9041ed51b
姚冬奕 <yaodongyi@yaodongyideMacBook-Pro.local>
2019-09-06 20:11:06 +0800

    优化sw,手写NetworkFirst缓存策略。

--- publicity-page/src/service-worker.js
+++ publicity-page/src/service-worker.js
@@ -5,10 +5,15 @@
  */
 // importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js'); // workbox
 importScripts('./static/js/workbox-sw.js'); // workbox
-let pwa_version = 'version:DRr-1567618480699'; // 提交的版本号，用以更新service-worker;
+let pwa_version = 'version:rmb-1567766549759'; // 提交的版本号，用以更新service-worker;
 let cacheList = ['/', 'index.html']; // 配置需要缓存的列表
+/**
+ * 每次serviceWorker文件有更新的时候，会先进入install, 然后触发activate, 更新缓存。
+ * fetch事件则是请求资源时触发。
+ */
 // 监听 service worker 的 install 事件
 self.addEventListener('install', function(event) {
+  console.log('install');
   // 在事件上接了一个 ExtendableEvent.waitUntil() 方法——这会确保 Service Worker 不会在 waitUntil() 里面的代码执行完毕之前安装完成。
   // 如果监听到了 service worker 已经安装成功的话，就会调用 event.waitUntil 回调函数
   event.waitUntil(
@@ -24,7 +29,6 @@ self.addEventListener('install', function(event) {
       })
   );
 });
-// https://www.easy-mock.com/mock/5ccec7de7ffbe958f9bc418b/
 
 // 我们可以在 install 的时候进行静态资源缓存，也可以通过 fetch 事件处理回调来代理页面请求从而实现动态资源缓存。
 // 两种方式可以比较一下：
@@ -32,28 +36,52 @@ self.addEventListener('install', function(event) {
 // on fetch 的优点是无需更改编译过程，也不会产生额外的流量，缺点是需要多一次访问才能离线可用。
 // 除了静态的页面和文件之外，如果对 Ajax 数据加以适当的缓存可以实现真正的离线可用， 要达到这一步可能需要对既有的 Web App 进行一些重构以分离数据和模板。
 self.addEventListener('fetch', function(event) {
+  console.log('fetch');
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      // 来来来，代理可以搞一些代理的事情
-      // 如果 Service Worker 有自己的返回，就直接返回，减少一次 http 请求
       if (response) {
-        return response;
-      }
-      // 如果 service worker 没有返回，那就得直接请求真实远程服务
-      var request = event.request.clone(); // 把原始请求拷过来
-      return fetch(request).then(function(httpRes) {
-        // http请求的返回已被抓到，可以处置了。
-        // 请求失败了，直接返回失败的结果就好了。。
-        if (!httpRes || httpRes.status !== 200) {
-          return httpRes;
+        /**
+         * 响应类型为 basic，亦即由自身发起的请求
+         * 如果是本地静态资源和服务端资源不是同一个域名
+         * 可以设置 response.type === basic为匹配规则，设置静态资源直接从serviceWorker取，除非缓存被清除
+         * @param {*} static 匹配规则
+         * @desc 所有静态资源直接从service-worker取,(注意：如果有动态验证码图片资源需要单独设置规则，或者从服务端取图片url)
+         * @returns response 静态资源
+         * @desc 这里设置静态直接返回的原因是，减少静态资源多进行一次请求。
+         * @desc 并且避免服务器返回缓慢导致静态资源显示缓慢(查看首页，并设置network的网络传输为slow可以直观的看出效果)。
+         */
+        let static = new RegExp(/.(js|css|png|jpe?g|gif|svg|htm|html|ico)/, 'g').test(response.url);
+        if (static) {
+          return response;
         }
-        // 请求成功的话，将请求缓存起来。
-        var responseClone = httpRes.clone();
-        caches.open(pwa_version).then(function(cache) {
-          cache.put(event.request, responseClone);
+      }
+      // 接口数据使用网络优先原则，以防数据不更新。对应workbox的NetworkFirst缓存策略。
+      var request = event.request.clone(); // 把原始请求拷过来
+      return fetch(request)
+        .then(function(httpRes) {
+          // http请求的返回已被抓到，可以处置了。
+          // 请求失败了，直接返回失败的结果就好了。。
+          // console.log(httpRes.url, new RegExp(/.(js|css|png|jpe?g|gif|svg|htm|html|ico)/, 'g').test(httpRes.url));
+          if (!httpRes || httpRes.status !== 200) {
+            return httpRes;
+          }
+          // 请求成功的话，将请求缓存起来。
+          var responseClone = httpRes.clone();
+          caches.open(pwa_version).then(function(cache) {
+            cache.put(event.request, responseClone);
+          });
+          // console.log('httpRes:', httpRes);
+          return httpRes;
+        })
+        .catch(err => {
+          // 如果 Service Worker 有自己的返回，就直接返回
+          if (response) {
+            console.log('请求服务器失败，使用缓存数据');
+            return response;
+          } else {
+            console.error(err);
+          }
         });
-        return httpRes;
-      });
     })
   );
 });
@@ -65,7 +93,13 @@ self.addEventListener('message', event => {
   }
 });
 
+/**
+ * 当serviceWorker被激活时触发
+ * @callback 缓存管理
+ * @desc active事件中通常做一些过期资源释放的工作
+ */
 self.addEventListener('activate', function(event) {
+  console.log('activate');
   //   console.log(event);
   event.waitUntil(
     Promise.all([
@@ -75,8 +109,9 @@ self.addEventListener('activate', function(event) {
       caches.keys().then(function(cacheList) {
         return Promise.all(
           cacheList.map(function(cacheName) {
+            // 判断是否有新版本的serverWorker，有的话删除掉上一次的缓存，使用新缓存
             if (cacheName !== pwa_version) {
-              console.log(cacheName, pwa_version);
+              console.log(`缓存已更新, 旧版本：${cacheName}, 新版本：${pwa_version}`);
               return caches.delete(cacheName);
             }
           })
@@ -85,4 +120,4 @@ self.addEventListener('activate', function(event) {
     ])
   );
 });
-console.log(pwa_version);
+// console.log(pwa_version);
