9fbeb192b91ce6ce817be0e1663c296c0f9d8ff8
姚冬奕 <yaodongyi@yaodongyideMacBook-Pro.local>
2019-09-06 20:09:09 +0800

    添加axios拦截器

--- publicity-page/src/assets/js/common/axios-pack.js
+++ publicity-page/src/assets/js/common/axios-pack.js
@@ -1,47 +1,42 @@
+/*
+ * @Author: yaodongyi
+ * @Date: 2019-07-26 16:12:05
+ * @Description:axios请求相应拦截器
+ */
 import axios from 'axios';
+import qs from 'qs';
 
-var service = axios.create({
-	baseURL: process.env.BASE_URL,
-	timeout: 5000
-});
+if (process.env.NODE_ENV === 'production') {
+  axios.defaults.baseURL = process.env.BASE_URL;
+} else {
+  axios.defaults.baseURL = '/api';
+}
 
-//service.interceptors.request.use(
-//	config => {
-//		config.headers = {
-//			"Accept": "application/json"
-//		};
-//		return config;
-//	},
-//	error => {
-//		console.log("requestError: " + error)
-//		return Promise.reject(err);
-//	}
-//);
-//
-//service.interceptors.response.use(
-//	response => {
-//		if(response.status == 200) {
-//			return response
-//		} else {
-//			return Promise.reject('error res.status=' + response.status)
-//		}
-//	},
-//	error => {
-//		/*报错处理 -->弹窗*/
-//		console.log("responseError: ", error.response)
-//		return Promise.reject(error)
-//	}
-//);
+axios.interceptors.request.use(
+  request => {
+    request.headers = {
+      // 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      'Content-Type': 'application/json; charset=UTF-8', //json
+      Accept: 'application/json'
+    };
+    // request.data = qs.stringify(request.data);
+    console.log(`%c 发送 ${request.url.replace(axios.defaults.baseURL, '')} `, 'background:#00CC6E;color:#ffffff', request);
+    return request;
+  },
+  err => {
+    return Promise.reject(err);
+  }
+);
 
-export function request(url, data, method) {
-	console.log(JSON.stringify(data))
-	return service.request({
-		url: url,
-		method: method,
-		data: data,
-		dataType: "json",
-		async: true,
-	}).then(function(data) {
-		return Promise.resolve(data.data);
-	});
-};
+axios.interceptors.response.use(
+  response => {
+    console.log(`%c 接收 ${response.config.url.replace(axios.defaults.baseURL, '')} `, 'background:#1E1E1E;color:#bada55', response);
+    return response.data;
+  },
+  error => {
+    console.error(error);
+    return Promise.reject(error);
+  }
+);
+
+export default axios;
