6f20cc3c6e85d7e24ab19d96debdcf464951a3b5
姚冬奕 <yaodongyi@yaodongyideMacBook-Pro.local>
2019-09-06 20:08:39 +0800

    添加api接口文件

--- publicity-page/src/assets/js/utils/api.js
+++ publicity-page/src/assets/js/utils/api.js
@@ -1,5 +1,18 @@
-import {request} from "../common/axios-pack";
+/*
+ * @Author: yaodongyi
+ * @Date: 2019-08-28 10:52:48
+ * @Description: api接口
+ */
+import axios from '../common/axios-pack';
+/**
+ * 测试接口
+ * @param {Object} params
+ * @api {post} https://www.easy-mock.com/mock/5ccec7de7ffbe958f9bc418b/all
+ */
+export const all_api = params => {
+  return axios.get(`all`, params);
+};
 
-// export var getSecurityCode = function(params)  {
-//   return request('/R-USER/c/getSecurityCode', params , "post");
-// };
+export const all_mock = params => {
+  return axios.get(`mock`, params);
+};
