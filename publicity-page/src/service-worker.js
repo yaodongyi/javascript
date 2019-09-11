e255872f23169a62c1756b78797a669eb0eb226c
姚冬奕 <yaodongyi@yaodongyideMacBook-Pro.local>
2019-09-11 12:11:04 +0800

    更新sw

--- publicity-page/src/service-worker.js
+++ publicity-page/src/service-worker.js
@@ -7,7 +7,7 @@
 importScripts('./static/js/workbox-sw.js'); // workbox
 // 请勿改写以下字段，swList，pwa_version，用于动态更新。
 var swList = ['index.html','home.html','info.html','about.html'];
-let pwa_version = 'version:hja-1568078830760'; // 提交的版本号，用以更新service-worker;
+let pwa_version = 'version:bFY-1568172578291'; // 提交的版本号，用以更新service-worker;
 let cacheList = ['/', ...swList]; // 配置需要缓存的列表
 /**
  * 每次serviceWorker文件有更新的时候，会先进入install, 然后触发activate, 更新缓存。
